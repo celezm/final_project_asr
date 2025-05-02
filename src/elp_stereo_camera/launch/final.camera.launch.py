3aede997c32c62dcd2524207764293cf5654eb45
Amanza17 <a.manzanares.2023@alumnos.urjc.es>
2025-05-02 18:13:31 +0200

    parser running in robot to adapt messages because of comms problems

---
+++ src/elp_stereo_camera/launch/final.camera.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    # Ruta al archivo launch de cada paquete
+    stereo_camera_launch = os.path.join(
+        get_package_share_directory('elp_stereo_camera'),
+        'launch',
+        'stereo_camera_launch.py'
+    )
+
+    yolo_launch = os.path.join(
+        get_package_share_directory('yolo_bringup'),
+        'launch',
+        'yolo.final.launch.py'
+    )
+
+    detection3d_launch = os.path.join(
+        get_package_share_directory('depth_worker'),
+        'launch',
+        'detection3d_yolo.launch.py'
+    )
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(stereo_camera_launch)
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(yolo_launch)
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(detection3d_launch)
+        )
+    ])
---
+++ src/nav_pruebas/src/nav_pruebas/DestinationParserNode.cpp
@@ -0,0 +1,61 @@
+
+#include <memory>
+
+#include "nav_pruebas/DestinationParserNode.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+
+namespace nav_pruebas
+{
+
+using std::placeholders::_1;
+
+DestinationParserNode::DestinationParserNode()
+: Node("destination_parser_node")
+{
+  destination_sub_ = create_subscription<std_msgs::msg::Int32>(
+    "/destination_topic", rclcpp::QoS(10),
+    std::bind(&DestinationParserNode::detection_callback, this, std::placeholders::_1));
+  
+  destination_pub_ = create_publisher<std_msgs::msg::Int32>(
+    "/real_destination", rclcpp::QoS(10));
+  
+}
+
+void
+DestinationParserNode::detection_callback(
+  const std_msgs::msg::Int32::ConstSharedPtr & msg)
+{
+
+  if (msg->data == -1) {
+    start_receiving_ = true;
+    end_receiving_ = false;
+  } else if (msg->data == 0) {
+    start_receiving_ = false;
+    end_receiving_ = true;
+  } 
+  
+  if (start_receiving_ && !end_receiving_) {
+    
+    if (msg->data != dest_id_){
+      dest_id_ = msg->data;
+    }
+    
+  } 
+  
+  if (end_receiving_) {
+    std_msgs::msg::Int32 destination_msg;
+    destination_msg.data = dest_id_;
+    if (dest_id_ != -1) {
+      destination_pub_->publish(destination_msg);
+    }
+    dest_id_ = -1;
+    start_receiving_ = false;
+    end_receiving_ = false;
+  }  
+
+
+
+}  
+
+}// namespace nav_pruebas
